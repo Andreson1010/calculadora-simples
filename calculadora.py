23312ad4b9c3e79df714fbf1e24263451cd04893
Francisco <anderson.pinheiro1010@gmail.com>
2024-08-30 18:49:38 -0300

    Adiciona a função de multiplicação à calculadora

--- calculadora.py
+++ calculadora.py
@@ -2,9 +2,13 @@
 def soma(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print("Soma de 2 + 3 =", soma(2, 3))
-
-
 def subtracao(a, b):
     return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Soma de 2 + 3 =", soma(2, 3))
+    print("Subtração de 5 - 2 =", subtracao(5, 2))
+    print("Multiplicação de 4 * 3 =", multiplicacao(4, 3))
