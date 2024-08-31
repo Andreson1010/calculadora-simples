8a73fc5632f2e611dd986f15ed5b10daf1b574bd
Francisco <anderson.pinheiro1010@gmail.com>
2024-08-31 12:20:40 -0300

    Adiciona o print da subtração a calculadora

--- calculadora.py
+++ calculadora.py
@@ -2,9 +2,11 @@
 def soma(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print("Soma de 2 + 3 =", soma(2, 3))
-
-
 def subtracao(a, b):
     return a - b
+
+if __name__ == "__main__":
+    print("Soma de 2 + 3 =", soma(2, 3))
+    print("Subtração de 3 - 2= ", subtracao(3, 2)
+
+	
