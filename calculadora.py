35aea4ddffda2c64dd487ccd8ebceb1c874d685c
Francisco <anderson.pinheiro1010@gmail.com>
2024-09-02 16:01:25 -0300

    adicionando a divisão

--- calculadora.py
+++ calculadora.py
@@ -8,7 +8,11 @@ def subtracao(a, b):
 def multiplicacao(a, b):
     return a * b
 
+def divisao (a, b):
+    return ( a // b)
+
 if __name__ == "__main__":
-    print("Soma de 2 + 3 =", soma(2, 3))
-    print("Subtração de 5 - 2 =", subtracao(5, 2))
+    print("A Soma de 2 + 3 =", soma(2, 3))
+    print("A Subtração de 5 - 2 =", subtracao(5, 2))
     print("Multiplicação de 4 * 3 =", multiplicacao(4, 3))
+    print(" A divisão de 8 / 4 =", divisao(8, 4) 
